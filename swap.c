8f74c3704a300bcc8a49af521f71d9ca04edc6ae
krummitz <amidbidee@gmail.com>
2021-10-26 21:01:01 +0100

    fixes

--- swap.c
+++ swap.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include "sort.h"
 
+/**
+ * swap - swaps two integers adjacent of each other in an array
+ *
+ * @x: pointer integer to the left
+ * @y: pointer integer to the right
+ */
+
 void swap(int *x, int *y)
 {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+	int temp = *x;
+	*x = *y;
+	*y = temp;
 }
