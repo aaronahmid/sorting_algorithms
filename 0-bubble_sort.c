a87bfb9c413ee4610281399f7facaad6e55ff14a
krummitz <amidbidee@gmail.com>
2021-10-26 22:36:47 +0100

    fixes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,39 +8,28 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, tmp;
 	int swapped; /* 1 -> true, 0 -> false */
 
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < (size - 1); i++)
 	{
 		swapped = 0;
 		for (j = 0; j < (size - i - 1); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				swapped = 1;
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
 
 		if (swapped == 0)
 			break;
 	}
-}
-
-/**
- * swap - swaps two integers adjacent of each other in an array
- *
- * @x: pointer integer to the left
- * @y: pointer integer to the right
- */
-void swap(int *x, int *y)
-{
-	int temp = *x;
-	*x = *y;
-	*y = temp;
 }
