f8e4936917ed27be16421864013d1ae1a32b21a9
krummitz <amidbidee@gmail.com>
2021-10-27 03:58:30 +0100

    fixes

--- sort.h
+++ sort.h
@@ -29,5 +29,6 @@ void swap(int *x, int *y);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
